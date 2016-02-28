f79f1bcdd5fa5c13f11053b6b134dcf5d40ce1e1
wasie <wasie@Wasies-Air.lan>
2016-02-28 13:03:20 -0800

    new test account added

--- tests/superqa_test.js
+++ tests/superqa_test.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "Verify 'About us' tab" : function (browser) {
+/*"Verify 'About us' tab" : function (browser) {
     browser
       .url("https://superqa.herokuapp.com/")
       .click(".aboutus-tab")
@@ -54,21 +54,43 @@ module.exports = {
       .pause(1000)
       .verify.containsText(".menuContainer", "Accounts")
   },
-
   "Verify \"register\"" : function (browser) {
     browser
       .url("https://superqa.herokuapp.com/")
       .waitForElementVisible('body', 1000)
-      .click('.register')
+      .click('.registrationForm')
       .pause(1000)
       .setValue('input[type=email]', 'wasie1@gmail.com')
       .setValue('input[name=fullname]', 'w1')
       .setValue(".registrationForm input:nth-child(3)", 'password')
       .setValue(".registrationForm input:nth-child(4)", 'password')
-      .click("input[value=register]")
+      .click("input[value=Register]")
       .end();
 
-
-
+  },*/
+  "Verify \"accounts\"" : function (browser) {
+    browser
+      .url("https://superqa.herokuapp.com/")
+      .waitForElementVisible('body', 1000)
+      .click(".login")
+      .pause(500)
+      .setValue('input[name=email]', 'wasietest1@gmail.com')
+      .setValue('input[type=password]', 'password')
+      .click(".button")
+      .pause(500)
+      .click(".accounts-tab")
+      .pause(500)
+      .click(".newAccountButton")
+      .pause(500)
+      .setValue("input[name=cohort]", "test")
+      .setValue("input[name=startDate]", "02/11/2016")
+      .setValue("input[name=endDate]", "02/20/2016")
+      .setValue("input[name=amount]", "1000.00")
+      .click(".addAccount")
+      .verify.containsText(".cohortMenu", "test")
+      .verify.containsText(".top li:first-child .mainLine a","test")
+      .jqueryClick(".cohortMenu li:contains('test') .removeAccount")
+      .pause(1000)
+      .acceptAlert()
   }
 };
